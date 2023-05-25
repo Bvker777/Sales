add1becbb781893aa2b4ede93212fc080951ce8b
Bakerlang Lyngdoh <bakerlang777@gmail.com>
2023-05-25 21:22:11 +0530

    update

--- src/scenes/bar/index.jsx
+++ src/scenes/bar/index.jsx
@@ -4,13 +4,12 @@ import Header from "../../components/Header";
 
 const Bar = () => {
   return (
-    // <Box m="20px">
-    //   <Header title="Bar Chart" subtitle="Simple Bar Chart" />
-    //   <Box height="75vh">
-    //     {/* <BarChart /> */}
-    //   </Box>
-    // </Box>
-    <></>
+    <Box m="20px">
+      <Header title="Bar Chart" subtitle="Simple Bar Chart" />
+      <Box height="75vh">
+        {/* <BarChart /> */}
+      </Box>
+    </Box>
   );
 };
 
