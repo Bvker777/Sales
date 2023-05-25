3eba88e7c2f13a6b90875f4743d46bc1aae67c88
Bakerlang Lyngdoh <bakerlang777@gmail.com>
2023-05-25 20:48:43 +0530

    sales

---
+++ src/scenes/contacts/index.jsx
@@ -0,0 +1,87 @@
+import { Box } from "@mui/material";
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+import { tokens } from "../../theme";
+import { sales } from "../../data/mockData";
+import Header from "../../components/Header";
+import { useTheme } from "@mui/material";
+
+const Contacts = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  const columns = [
+    { field: "id", headerName: "ID", flex: 0.5 },
+    { field: "salesId", headerName: "Sales ID", flex: 1 },
+    {
+      field: "bookTitle",
+      headerName: "Book Title",
+      flex: 3,
+      cellClassName: "name-column--cell",
+    },
+    {
+      field: "qty",
+      headerName: "Qty",
+      type: "number",
+      headerAlign: "left",
+      align: "left",
+      flex: 0.5,
+    },
+    {
+      field: "date",
+      headerName: "Date",
+      flex: 1,
+    },
+
+    {
+      field: "amount",
+      headerName: "Amount",
+      flex: 1,
+    },
+  ];
+
+  return (
+    <Box m="20px">
+      <Header title="Sales" subtitle="Total sales" />
+      <Box
+        m="40px 0 0 0"
+        height="75vh"
+        sx={{
+          "& .MuiDataGrid-root": {
+            border: "none",
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: "none",
+          },
+          "& .name-column--cell": {
+            color: colors.greenAccent[300],
+          },
+          "& .MuiDataGrid-columnHeaders": {
+            backgroundColor: colors.blueAccent[700],
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: colors.primary[400],
+          },
+          "& .MuiDataGrid-footerContainer": {
+            borderTop: "none",
+            backgroundColor: colors.blueAccent[700],
+          },
+          "& .MuiCheckbox-root": {
+            color: `${colors.greenAccent[200]} !important`,
+          },
+          "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+            color: `${colors.grey[100]} !important`,
+          },
+        }}
+      >
+        <DataGrid
+          rows={sales}
+          columns={columns}
+          components={{ Toolbar: GridToolbar }}
+        />
+      </Box>
+    </Box>
+  );
+};
+
+export default Contacts;
